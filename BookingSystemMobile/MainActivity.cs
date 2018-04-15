8740717eecb10b269f0bbef9308ab0fe632d96bd
GLEESON <N3024253@live.tees.ac.uk>
2018-04-15 16:00:43 +0100

    Started creating xamarin application.

---
+++ BookingSystemMobile/MainActivity.cs
@@ -0,0 +1,181 @@
+using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using Android.Support.V4.Widget;
+using Android.Views;
+
+using BookingSystemMobile.Fragments;
+using Android.Support.V7.App;
+using Android.Support.V4.View;
+using Android.Support.Design.Widget;
+using System.Threading.Tasks;
+using LibBookingService.Dtos;
+using System.Net;
+using System;
+using System.IO;
+using System.Json;
+using LibBookingService;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace BookingSystemMobile
+{
+    [Activity(Label = "@string/app_name", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, Icon = "@drawable/Icon")]
+    public class MainActivity : AppCompatActivity
+    {
+        private readonly string _webApiBaseUrl = "http://localhost:64577/api/";
+        private readonly string _authServiceBaseUrl = "http://localhost:64392/";
+
+        DrawerLayout drawerLayout;
+        NavigationView navigationView;
+        
+        IMenuItem previousItem;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.main);
+            var toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
+            if (toolbar != null)
+            {
+                SetSupportActionBar(toolbar);
+                SupportActionBar.SetDisplayHomeAsUpEnabled(true);
+                SupportActionBar.SetHomeButtonEnabled(true);
+            }
+
+            drawerLayout = FindViewById<DrawerLayout>(Resource.Id.drawer_layout);
+
+            //Set hamburger items menu
+            SupportActionBar.SetHomeAsUpIndicator(Resource.Drawable.ic_menu);
+
+            //setup navigation view
+            navigationView = FindViewById<NavigationView>(Resource.Id.nav_view);
+
+            //handle navigation
+            navigationView.NavigationItemSelected += (sender, e) =>
+            {
+                if (previousItem != null)
+                    previousItem.SetChecked(false);
+
+                navigationView.SetCheckedItem(e.MenuItem.ItemId);
+
+                previousItem = e.MenuItem;
+
+                switch (e.MenuItem.ItemId)
+                {
+                    case Resource.Id.nav_home_1:
+                        ListItemClicked(0);
+                        break;
+                    case Resource.Id.nav_home_2:
+                        ListItemClicked(1);
+                        break;
+                }
+
+
+                drawerLayout.CloseDrawers();
+            };
+
+
+            //if first time you will want to go ahead and click first item.
+            if (savedInstanceState == null)
+            {
+                navigationView.SetCheckedItem(Resource.Id.nav_home_1);
+                ListItemClicked(0);
+            }
+        }
+
+        int oldPosition = -1;
+        private void ListItemClicked(int position)
+        {
+            //this way we don't load twice, but you might want to modify this a bit.
+            if (position == oldPosition)
+                return;
+
+            oldPosition = position;
+
+            Android.Support.V4.App.Fragment fragment = null;
+            switch (position)
+            {
+                case 0:
+                    fragment = Fragment1.NewInstance();
+                    break;
+                case 1:
+                    fragment = Fragment2.NewInstance();
+                    break;
+            }
+
+            SupportFragmentManager.BeginTransaction()
+                .Replace(Resource.Id.content_frame, fragment)
+                .Commit();
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            switch (item.ItemId)
+            {
+                case Android.Resource.Id.Home:
+                    drawerLayout.OpenDrawer(GravityCompat.Start);
+                    return true;
+            }
+            return base.OnOptionsItemSelected(item);
+        }
+
+        private async Task<JsonValue> GetToken()
+        {
+            HttpWebRequest request = (HttpWebRequest) HttpWebRequest.Create(new Uri(_authServiceBaseUrl + "token"));
+            request.ContentType = "application/json";
+            request.Method = "GET";
+
+            using (WebResponse response = await request.GetResponseAsync())
+            {
+                using (Stream stream = response.GetResponseStream())
+                {
+                    JsonValue jsonDoc = await Task.Run(() => JsonObject.Load(stream));
+                    Console.Out.WriteLine("Response: {0}", jsonDoc.ToString());
+
+                    return jsonDoc;
+                }
+            }
+        }
+
+        private async Task<T> ExecuteRequestAsync<T>(HttpRequestMessage request) where T : Dto
+        {
+            using (HttpClient _client = new HttpClient())
+            {
+                _client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                JsonSerializerSettings _serializerSettings = new JsonSerializerSettings
+                {
+                    NullValueHandling = NullValueHandling.Ignore,
+                    MissingMemberHandling = MissingMemberHandling.Ignore
+                };
+
+                HttpResponseMessage response = await _client.SendAsync(request);
+                response.EnsureSuccessStatusCode();
+                string content = await response.Content.ReadAsStringAsync();
+                return JsonConvert.DeserializeObject<T>(content, _serializerSettings);
+            }
+        }
+
+        private async Task<List<T>> ExecuteRequestAsyncList<T>(HttpRequestMessage request) where T : Dto
+        {
+            using (HttpClient _client = new HttpClient())
+            {
+                _client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                JsonSerializerSettings _serializerSettings = new JsonSerializerSettings
+                {
+                    NullValueHandling = NullValueHandling.Ignore,
+                    MissingMemberHandling = MissingMemberHandling.Ignore
+                };
+
+                HttpResponseMessage response = await _client.SendAsync(request);
+                response.EnsureSuccessStatusCode();
+                string content = await response.Content.ReadAsStringAsync();
+                return JsonConvert.DeserializeObject<List<T>>(content, _serializerSettings);
+            }
+        }
+    }
+}
+
